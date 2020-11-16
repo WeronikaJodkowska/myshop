b765b0e1d7213c1539334dfd9c432b03d9c1343b
WeronikaJodkowska <veronica.jodkowska@gmail.com>
2020-11-15 22:40:36 -0800

    Applying a coupon to the shopping cart

--- coupons/models.py
+++ coupons/models.py
@@ -8,8 +8,8 @@ class Coupon(models.Model):
     valid_from = models.DateTimeField()
     valid_to = models.DateTimeField()
     discount = models.IntegerField(
-        validators=[MinValueValidator(0),
-                    MaxValueValidator(100)])
+                   validators=[MinValueValidator(0),
+                               MaxValueValidator(100)])
     active = models.BooleanField()
 
     def __str__(self):
--- coupons/views.py
+++ coupons/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.views.decorators.http import require_POST
-
 from .models import Coupon
 from .forms import CouponApplyForm
 
@@ -14,7 +13,7 @@ def coupon_apply(request):
         code = form.cleaned_data['code']
         try:
             coupon = Coupon.objects.get(code__iexact=code,
-                                        valid_from_lte=now,
+                                        valid_from__lte=now,
                                         valid_to__gte=now,
                                         active=True)
             request.session['coupon_id'] = coupon.id
